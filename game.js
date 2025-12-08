77f3e056cf7789f79b1996cd7e3e5c95c474375f
Mudasir <thisismjeyz@gmail.com>
2025-12-08 20:21:00 +0500

    initial commit

---
+++ game.js
@@ -0,0 +1,20 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+
+    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+
+    userChosenColour = $("#" + randomChosenColour).click(function () {
+    alert("i am Clicked")
+    console.log(userChosenColour)
+});
+
+}
+
